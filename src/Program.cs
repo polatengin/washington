e12f487c257a6041d40c7ae94acd72e9069ad237
Engin Polat <polatengin@hotmail.com>
2022-06-23 23:07:21 +0000

    adding file argument to the cli

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,19 @@
+using System.CommandLine;
 public class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        var fileOption = new Option<FileInfo?>(name: "--file", description: "The file to read and display on the console.") { IsRequired = true };
+
+        var rootCommand = new RootCommand("Bicep and ARM Cost Estimator");
+
+        rootCommand.AddOption(fileOption);
+
+        rootCommand.SetHandler(async (file) =>
+        {
+            Console.WriteLine($"File: {file}");
+        }, fileOption);
+
+        return await rootCommand.InvokeAsync(args);
     }
 }
