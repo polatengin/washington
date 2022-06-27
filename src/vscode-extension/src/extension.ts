186cc01c25032e757bfdc6f54a00a0fd4bec364a
Engin Polat <polatengin@hotmail.com>
2022-06-27 01:56:11 +0000

    renaming extension command

--- src/vscode-extension/src/extension.ts
+++ src/vscode-extension/src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-  context.subscriptions.push(vscode.commands.registerCommand('azure-cost-estimator.helloWorld', () => {
+  context.subscriptions.push(vscode.commands.registerCommand('azure-cost-estimator.estimate', () => {
     vscode.window.showInformationMessage('Hello World from azure-cost-estimator!');
   }));
 
