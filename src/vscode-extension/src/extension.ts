e143231c237f356467ec631dd73bcdb595a21383
Engin Polat <polatengin@hotmail.com>
2022-06-28 01:49:49 +0000

    adding number generator function

--- src/vscode-extension/src/extension.ts
+++ src/vscode-extension/src/extension.ts
@@ -1,5 +1,14 @@
 import * as vscode from 'vscode';
 
+const generateId = function* () {
+  let i = 0;
+  while (true) {
+    yield i++;
+  }
+};
+
+const generator = generateId();
+
 export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(vscode.commands.registerCommand('azure-cost-estimator.estimate', () => {
