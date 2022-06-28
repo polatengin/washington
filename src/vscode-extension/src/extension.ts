c59138faa54c404a9706d1d831c061288133560f
Engin Polat <polatengin@hotmail.com>
2022-06-28 01:50:27 +0000

    adding statusbar messages

--- src/vscode-extension/src/extension.ts
+++ src/vscode-extension/src/extension.ts
@@ -11,8 +11,17 @@ const generator = generateId();
 
 export function activate(context: vscode.ExtensionContext) {
 
-  context.subscriptions.push(vscode.commands.registerCommand('azure-cost-estimator.estimate', () => {
-    vscode.window.showInformationMessage('Hello World from azure-cost-estimator!');
+  context.subscriptions.push(vscode.commands.registerCommand('azure-cost-estimator.estimate', (uri: vscode.Uri) => {
+
+    const window = vscode.window;
+
+    const file = uri.fsPath || window.activeTextEditor?.document.fileName;
+
+    window.setStatusBarMessage(`Estimating cost of Azure resources for ${file}`, 3000);
+
+    window.setStatusBarMessage(`Estimated cost of Azure resources for ${file}`, 3000);
+
+    window.showInformationMessage('Hello World from azure-cost-estimator!');
   }));
 
 }
