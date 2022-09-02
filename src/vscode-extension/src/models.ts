627416440de94248f09caf34c248bb211e27d7a3
Engin Polat <polatengin@hotmail.com>
2022-09-02 00:41:30 +0000

    adding common resource model

--- src/vscode-extension/src/models.ts
+++ src/vscode-extension/src/models.ts
@@ -4,3 +4,12 @@ interface ARMTemplateJson {
   resources: Resource[];
   parameters: { [key in string]: { type: string, defaultValue: string, maxValue: string, minValue: string } };
 }
+
+interface Resource {
+  name: string;
+  type: string;
+  apiVersion: string;
+  properties: any;
+  location: string;
+  [key: string]: any;
+}
