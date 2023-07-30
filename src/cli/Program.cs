65f9fef2391dc14e9ab4838b8b485d3a72375736
polatengin <enpolat@microsoft.com>
2023-07-30 15:37:15 +0000

    renaming root command

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -8,7 +8,7 @@ public class Program
   {
     var fileOption = new Option<FileInfo?>(name: "--file", description: "The file to read and display on the console.") { IsRequired = true };
 
-    var rootCommand = new RootCommand("Bicep and ARM Cost Estimator");
+    var rootCommand = new RootCommand("Azure Cost Estimator");
 
     rootCommand.AddOption(fileOption);
 
