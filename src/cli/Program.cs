5c715b184e7e3c60ac24f2eec539b2cbb9f1f772
polatengin <enpolat@microsoft.com>
2024-10-07 14:27:41 -0700

    adding helper method to evaluate format expressions

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -81,6 +81,33 @@ public class Program
       resource.estimatedMonthlyCost = perhour * 24 * 30 ?? 0;
 
       Console.WriteLine($"{resource.name}({resource.serviceName}/{resource.size}) estimated monthly cost: {string.Format("{0:C2}", resource.estimatedMonthlyCost)}");
+  }
+
+  private static string EvaluateFormatExpression(string input)
+  {
+    var pattern = @"\[\s*format\(\s*'([^']+)'(?:\s*,\s*'([^']+)')*\s*\)\s*\]";
+
+    var match = Regex.Match(input, pattern);
+
+    if (match.Success)
+    {
+      var format = match.Groups[1].Value;
+
+      var argumentPattern = @"'([^']+)'";
+
+      var argumentMatches = Regex.Matches(input, argumentPattern);
+
+      var arguments = new List<string>();
+      for (int i = 1; i < argumentMatches.Count; i++)
+      {
+        arguments.Add(argumentMatches[i].Groups[1].Value);
+      }
+
+      return string.Format(format, arguments.ToArray());
+    }
+    else
+    {
+      return input;
     }
   }
 }
