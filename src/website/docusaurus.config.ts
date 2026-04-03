2dbd4a7862cdb05d65be385b1b7b7572cc16825b
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-04-03 14:58:52 -0700

    fix: update social card image path and replace with new image

--- src/website/docusaurus.config.ts
+++ src/website/docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
